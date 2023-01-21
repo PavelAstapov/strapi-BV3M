c2c6c275c1cf4eadbec1c357b061e795dc969935
Pavel Astapov <p.astapov@itransition.com>
2023-01-21 18:09:58 +0300

    updated strapi

--- config/plugins.js
+++ config/plugins.js
@@ -18,4 +18,49 @@ module.exports = ({ env }) => ({
         },
       },
     },
+    comments: {
+      enabled: true,
+      config: {
+        badWords: false,
+        moderatorRoles: ["Authenticated"],
+        approvalFlow: ["api::page.page"],
+        entryLabel: {
+          "*": ["Title", "title", "Name", "name", "Subject", "subject"],
+          "api::page.page": ["MyField"],
+        },
+        reportReasons: {
+          MY_CUSTOM_REASON: "MY_CUSTOM_REASON",
+        },
+        gql: {
+          // ...
+        },
+      },
+    },
+    menus: {
+      config: {
+        maxDepth: 3,
+      },
+    },
+    seo: {
+      enabled: true,
+    },
+    publisher: {
+      enabled: true,
+    },
+    graphql: {
+      config: {
+        endpoint: '/graphql',
+        shadowCRUD: true,
+        playgroundAlways: false,
+        depthLimit: 7,
+        amountLimit: 100,
+        apolloServer: {
+          tracing: false,
+        },
+      },
+    },
+    'editorjs': {
+      enabled: true,
+      resolve: './src/plugins/strapi-plugin-react-editorjs'
+    },
   });
